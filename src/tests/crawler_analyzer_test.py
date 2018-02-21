8eae7ca6c6a40600a27d37948ee71e9e773deb73
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-02-21 19:05:39 +0100

    compare results of crawler with paper

--- src/tests/crawler_analyzer_test.py
+++ src/tests/crawler_analyzer_test.py
@@ -1,3 +1,4 @@
+from csv import DictReader
 import os
 import pytest
 
@@ -9,8 +10,31 @@ def is_stage_clean(stage):
     return not os.path.exists(path) or os.listdir(path) == []
 
 
+def results_path(stage):
+    path = stage._stagepath
+    return path + "results.csv"
+
+
 @pytest.mark.first
 def test_graph_crawler():
-    crawler = GraphCrawler(graph_filter_func=lambda x: x["nodes"] < 100000)
     assert is_stage_clean(crawler)
+
+    def graph_filter_func(graph_properties):
+        return int(graph_properties["Nodes"]) < 10000
+    crawler = GraphCrawler(graph_filter_func=graph_filter_func)
     crawler.execute()
+
+    with open(results_path(crawler)) as crawl_results:
+        crawled_graphs = [
+            i["Graph"] for i in DictReader(crawl_results)
+        ]
+    print(crawled_graphs)
+
+    with open("../data/paper-features.csv") as paper_features:
+        filtered_paper_graphs = [
+            i["Graph"] for i in DictReader(paper_features)
+            if graph_filter_func(i)
+        ]
+    print(filtered_paper_graphs)
+
+    assert set(crawled_graphs) >= set(filtered_paper_graphs)
