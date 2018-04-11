28efdf2fef2db26d41ee6cc82da04c0ad105327e
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-04-12 00:08:49 +0200

    simplify test and check if index exists

--- src/tests/feature_extractor_cleaner_test.py
+++ src/tests/feature_extractor_cleaner_test.py
@@ -37,30 +37,27 @@ def test_feature_extractor_and_cleaner():
     cleaner.execute()
 
     def get_hash_dicts(file):
-        keys = ["Graph", "Type", "Model", "Nodes", "Edges"]
-        for i in DictReader(file):
-            yield HashDict(i)
-    with open(cleaner.resultspath) as file:
-        extracted_features = set(get_hash_dicts(file))
+        return [HashDict(i) for i in DictReader(file)]
 
-    stage1_names = [i["Name"] for i in crawler_dicts]
+    with open(cleaner.resultspath) as file:
+        extracted_features = get_hash_dicts(file)
+
     with open(extractor._stagepath + "../paper-features.csv") as file:
-        paper_features = set(
-            i for i in get_hash_dicts(file)
-            if i["Graph"] in stage1_names
-        )
+        paper_features = get_hash_dicts(file)
 
     def get_tuple(x):
         return (x["Graph"], x["Model"])
-    paper_list = list(paper_features)
-    paper_tuples = [get_tuple(j) for j in paper_list]
+
+    paper_tuples = [get_tuple(j) for j in paper_features]
     for i in extracted_features:
         if i["Model"] in ["BA circle", "BA full"]:
             continue
-        a = 0
+        if get_tuple(i) not in paper_tuples:
+            continue
 
+        a = 0
         index = paper_tuples.index(get_tuple(i))
-        j = paper_list[index]
+        j = paper_features[index]
         for key in i:
             if key in j:
                 try:
