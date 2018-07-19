37ffe77b2de8426a662f041a662207221fbd1039
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2018-07-19 13:52:46 +0200

    Add files for self-check experiments

---
+++ src/classifier_self_check.py
@@ -0,0 +1,105 @@
+from abstract_stage import AbstractStage
+from helpers import dicts_to_df, format_feature_df
+from helpers.classification import Classification
+from helpers.feature_sets import get_all_feature_sets
+
+import collections
+import pandas
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+def classification_experiment(df, network_models, features_collection, printing=False):
+    Y = df["Model"]
+
+    models = {
+        "svm": {
+            "model": make_pipeline(StandardScaler(), SVC()),  # kernel="rbf", cache_size=500
+            "cv_grid": {
+                "svc__C": [10 ** exp for exp in range(5)],
+                "svc__gamma": [10 ** -exp for exp in range(5)]
+            }
+        },
+        # {
+        #     "model": DummyClassifier,
+        #     "params": {"strategy": "most_frequent"}
+        # }
+    }
+
+    accuracies = pandas.DataFrame()
+
+    cv_best_misclassified_masks = collections.defaultdict(dict)
+    model_masks = {}
+
+    for network_model in network_models:
+        if printing:
+            print("Model", network_model)
+        network_model_mask = (df["Model"] == network_model) | (df["Model"] == network_model + "-second")
+        model_masks[network_model] = network_model_mask
+
+        for features_name, features in sorted(features_collection.items()):
+            if printing:
+                print("Feature set", features_name)
+            X = df[features]
+            best_accuracy = 0
+            for model_name, model in models.items():
+                c = Classification(
+                    X.loc[network_model_mask],
+                    Y.loc[network_model_mask],
+                    **model
+                )
+                cv_acc = c.results["cv"]["accuracy"]
+                accuracies.loc[network_model, features_name] = cv_acc
+                if cv_acc > best_accuracy:
+                    best_accuracy = cv_acc
+                    cv_best_misclassified_masks[features_name][network_model] = c.results["cv"]["mask"]
+    else:
+        return (accuracies, model_masks, cv_best_misclassified_masks)
+
+
+class ClassifierSelfCheck(AbstractStage):
+    _stage = "4-classification_results"
+
+    def __init__(self, features):
+        super(Classifier, self).__init__()
+        self.features = features
+
+    def _execute(self):
+        df = dicts_to_df(self.features)
+        format_feature_df(df)
+
+        df_real = df[df["Model"] == "real-world"]
+        print(collections.Counter(df["Model"]))
+
+        small_avg_degree = df_real["Centrality.Degree.Location.Arithmetic Mean"] <= 30
+
+        filters = {
+            "all": [True] * len(df_real),
+            "avg-degree-le-30": small_avg_degree,
+            "avg-degree-gt-30": ~small_avg_degree,
+            "socfb":     df_real["Type"] == "socfb",
+            "not-socfb": df_real["Type"] != "socfb"
+        }
+
+        format_str = "{:20}{:>5}"
+        #network_models = sorted(set(df["Model"])-set(["real-world"]))
+        network_models = sorted(["real-world", "ER", "BA circle", "BA full", "chung-lu", "hyperbolic"])
+
+        for filtername, filterdf in sorted(filters.items()):
+            graphs = sorted(df_real[filterdf]["Graph"])
+            print(format_str.format(filtername, len(graphs)))
+
+            features_collection = get_all_feature_sets(df, graphs)
+            sub_df = df.loc(axis=0)[:, graphs, :]
+            accuracies, model_masks, cv_best_misclassified_masks = \
+                classification_experiment(
+                    sub_df,
+                    network_models,
+                    features_collection,
+                    printing=False)
+            accuracies.to_csv(
+                self._stagepath + "accuracies/" + filtername + ".csv",
+                header=True,
+                index_label="features"
+            )
---
+++ src/main_self_check.py
@@ -0,0 +1,48 @@
+import os
+
+from graph_crawler import GraphCrawler
+from feature_extractor_self_check import FeatureExtractorSelfCheck
+from feature_cleaner import FeatureCleaner
+from classifier_self_check import ClassifierSelfCheck
+from csv import DictReader
+
+
+def run_stages(stages, initial_input_data=None, check_clean=True, **initial_kwargs):
+    input_data = initial_input_data
+    for i, stage in enumerate(stages):
+        print("### STAGE", stage.__name__, "###")
+        if check_clean:
+            stage_path = stage._stagepath
+            if os.path.exists(stage_path) and not os.listdir(stage_path) == []:
+                raise RuntimeError(
+                    "Make sure the folder %s is empty." % stage_path
+                )
+        if isinstance(input_data, str):
+            with open(input_data) as input_dicts_file:
+                input_dicts = list(DictReader(input_dicts_file))
+        else:
+            input_dicts = input_data
+        if input_dicts:
+            if i == 0:
+                stage(input_dicts, **initial_kwargs).execute()
+            else:
+                stage(input_dicts).execute()
+        else:
+            if i == 0:
+                stage(**initial_kwargs).execute()
+            else:
+                stage().execute()
+        input_data = stage.resultspath
+
+
+run_stages(
+    [
+        #GraphCrawler, 
+        FeatureExtractorSelfCheck, 
+        FeatureCleaner, 
+        ClassifierSelfCheck
+    ],
+    #FeatureCleaner.resultspath,
+    GraphCrawler.resultspath,
+    check_clean=False
+)
