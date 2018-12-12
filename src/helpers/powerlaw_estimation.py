13e36dc3f60295e00f86aa3a43772c8e4fc30e2b
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2018-12-12 11:25:17 +0100

    Add new tail estimation

--- src/helpers/powerlaw_estimation.py
+++ src/helpers/powerlaw_estimation.py
@@ -1,5 +1,15 @@
 import powerlaw
+import helpers.tail_estimation as pl
+
+#def powerlaw_fit(degrees):
+#    fit = powerlaw.Fit(degrees, fit_method='Likelihood', verbose=False)
+#    return fit.alpha
 
 def powerlaw_fit(degrees):
-    fit = powerlaw.Fit(degrees, fit_method='Likelihood', verbose=False)
-    return fit.alpha
+    degrees.sort(reverse=True)
+    try:
+        result = pl.hill_estimator(degrees)
+        alpha = 1 + 1 / result[3]
+    except:
+        alpha = 2.1
+    return alpha
